47d59d61b4cc3ab624d9fa920d40f2c1bab636fa
Mark <rabidwaffle@gmail.com>
2015-03-27 22:41:37 -0400

    Add boost program options skeleton

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
-
+#include <iostream>
+#include <cstdlib>
+#include <boost/program_options.hpp>
 
 int main(int argc, char* args[])
 {
-    
+  boost::program_options::options_description desc( "Options" );
+  desc.add_options()
+    ("help", "print help text")
+    ;
+  boost::program_options::variables_map vm;
+  boost::program_options::store(boost::program_options::parse_command_line(ac,av,desc),vm);
+  boost::program_options::notify(vm);
+  if( vm.count("help") )
+  {
+    std::cout << desc << std::endl;
+    exit( EXIT_SUCCESS );
+  }
 }
