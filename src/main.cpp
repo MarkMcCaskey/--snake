719e5a6870c250dd8ad816ad475cd7cb072d2a58
Mark <rabidwaffle@gmail.com>
2015-03-28 23:33:56 -0400

    Improve po skeleton

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,31 @@
 #include <iostream>
 #include <cstdlib>
-#include <boost/program_options.hpp>
+#include "boost/program_options.hpp"
 
-int main(int argc, char* args[])
+namespace po = boost::program_options;
+
+int main(int argc, char* argv[])
 {
-  boost::program_options::options_description desc( "Options" );
+  po::options_description desc( "Options" );
   desc.add_options()
     ("help", "print help text")
     ;
-  boost::program_options::variables_map vm;
-  boost::program_options::store(boost::program_options::parse_command_line(ac,av,desc),vm);
-  boost::program_options::notify(vm);
-  if( vm.count("help") )
-  {
-    std::cout << desc << std::endl;
-    exit( EXIT_SUCCESS );
+
+  po::variables_map vm;
+
+  try{
+    po::store(po::parse_command_line(argc,argv,desc),vm);
+    if( vm.count("help") )
+    {
+	std::cout << desc << std::endl;
+	exit( EXIT_SUCCESS );
+    }
+    po::notify(vm);
   }
+  catch(po::error &e )
+    {
+      std::cerr << "Error: " e.what() << std::endl;
+      exit( EXIT_FAILURE );
+    }
+  exit( EXIT_SUCCESS );
 }
