509f09360d93a98f3fe40503a651b52eac44514e
Mark <rabidwaffle@gmail.com>
2015-04-08 23:53:43 -0400

    Add basic ncurses skeleton

---
+++ src/ncurses.cpp
@@ -0,0 +1,43 @@
+#include <ncurses.h>
+#include <cstdlib>
+
+int graphics_loop( void )
+{
+  //set up
+  initscr();
+  clear();
+  noecho();
+  cbreak();
+
+  char input = '';
+  bool loop = true;
+  while( loop )
+    {
+      input = getch();
+      switch( input )
+	{
+	case 'q':
+	case 'Q':
+	  loop = false;
+	  break;
+	case KEY_RIGHT:
+	  //logic here
+	  break;
+	case KEY_LEFT:
+	  //logic here
+	  break;
+	case KEY_DOWN:
+	  //logic here
+	  break;
+	case KEY_UP:
+	  //logic here
+	  break;
+	}
+      refresh();
+    }
+
+  //end
+  endwin();
+
+  return EXIT_SUCCESS;
+}
